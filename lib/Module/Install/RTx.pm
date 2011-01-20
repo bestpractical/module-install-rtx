17182da07585e5c6c5334f3d3bc720b754cdd9ca
Ben Walton <bwalton@artsci.utoronto.ca>
2011-01-20 15:10:10 -0500

    Make postamble install targets honour DESTDIR
    Previously, the postamble install targets generated in the Makefile
    after calling perl Makefile.PL didn't honour DESTDIR, which led to
    problems when building using this module to package in rpms, etc.
    
    This change sees DESTDIR properly honoured at install time.
    
    Signed-off-by: Ben Walton <bwalton@artsci.utoronto.ca>

--- lib/Module/Install/RTx.pm
+++ lib/Module/Install/RTx.pm
@@ -99,7 +99,7 @@ sub RTx {
     my %index = map { $_ => 1 } @INDEX_DIRS;
     $self->no_index( directory => $_ ) foreach grep !$index{$_}, @DIRS;
 
-    my $args = join ', ', map "q($_)", map { ($_, $path{$_}) }
+    my $args = join ', ', map "q($_)", map { ($_, "\$(DESTDIR)$path{$_}") }
         grep $subdirs{$_}, keys %path;
 
     print "./$_\t=> $path{$_}\n" for sort keys %subdirs;

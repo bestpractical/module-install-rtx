6b3a2b2d434bd87102cc32564ac73d787533f756
sunnavy <sunnavy@bestpractical.com>
2020-07-09 04:24:44 +0800

    Notify user if the extension is already cored

--- lib/Module/Install/RTx.pm
+++ lib/Module/Install/RTx.pm
@@ -76,6 +76,22 @@ sub RTx {
         $self->requires_rt('4.0.0');
     }
 
+    my $package = $name;
+    $package =~ s/-/::/g;
+    if ( $RT::CORED_PLUGINS{$package} ) {
+        my ($base_version) = $RT::VERSION =~ /(\d+\.\d+\.\d+)/;
+        die <<"EOT";
+
+**** Error: Your installed version of RT ($RT::VERSION) already
+            contains this extension in core, so you don't need to
+            install it.
+
+            Check https://docs.bestpractical.com/rt/$base_version/RT_Config.html
+            to configure it.
+
+EOT
+    }
+
     # Installation locations
     my %path;
     my $plugin_path;
